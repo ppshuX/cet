a64345b1b7d7517fa4fb3f95e85abc9384cb6007
wenxiao Lv <2064747320@qq.com>
2025-05-19 10:27:10 +0800

    first_commit

---
+++ cet/cetapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CetappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cetapp'
---
+++ cet/cetapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def listening(request):
+    return render(request, 'listening.html')
+
+def reading(request):
+    return render(request, 'reading.html')
+
+def writing(request):
+    return render(request, 'writing.html')
+
+def translate(request):
+    return render(request, 'translate.html')
