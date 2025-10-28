e417e9566c9fe9e4147be5b7f61d1ffdb8ad5e99
wenxiao Lv <2064747320@qq.com>
2025-10-28 20:27:04 +0800

    feat: add redirect for all old trip URLs to new trip pages

--- trips/views/trip_views.py
+++ trips/views/trip_views.py
@@ -1,32 +1,14 @@
 """
 旅行页面相关视图
 """
-from django.shortcuts import render
-from ..models import Comment, SiteStat
+from django.shortcuts import redirect
+from ..models.trip import Trip
 
 
-def trip_page(request):
-    """厦门旅行页面"""
-    stats = SiteStat.objects.filter(page='trip').first()
-    if not stats:
-        stats = SiteStat.objects.create(page='trip')
-    stats.views += 1
-    stats.save()
-    comments = Comment.objects.filter(page='trip').order_by('-timestamp')
-    return render(request, 'trips/trip.html', {
-        'comments': comments,
-        'stats': stats,
-    })
-
-
-def trip1(request):
-    """三岔河一日游页面 - 重定向到新的旅行计划"""
-    from django.shortcuts import redirect
-    from ..models.trip import Trip
-    
-    # 尝试查找对应 slug 的旅行计划
+def redirect_old_trip(request, old_page_name):
+    """将旧URL重定向到新的旅行计划"""
     try:
-        trip = Trip.objects.filter(slug='trip1').first()
+        trip = Trip.objects.filter(slug=old_page_name).first()
         if trip:
             # 重定向到新的旅行计划URL
             return redirect(f'/trip/{trip.slug}/', permanent=True)
@@ -37,28 +19,27 @@ def trip1(request):
     return redirect('/', permanent=False)
 
 
+def trip_page(request):
+    """厦门旅行页面 - 重定向"""
+    return redirect_old_trip(request, 'trip')
+
+
+def trip1(request):
+    """三岔河一日游页面 - 重定向"""
+    return redirect_old_trip(request, 'trip1')
+
+
 def trip2(request):
-    """曲靖二日游页面"""
-    return trip_page_generic(request, 'trip2')
+    """曲靖二日游页面 - 重定向"""
+    return redirect_old_trip(request, 'trip2')
 
 
 def trip4(request):
-    """长沙慢旅行页面"""
-    return trip_page_generic(request, 'trip4')
+    """长沙慢旅行页面 - 重定向"""
+    return redirect_old_trip(request, 'trip4')
 
 
 def trip_page_generic(request, page_name):
-    """通用旅行页面渲染函数"""
-    stats = SiteStat.objects.filter(page=page_name).first()
-    if not stats:
-        stats = SiteStat.objects.create(page=page_name)
-    stats.views += 1
-    stats.save()
-    comments = Comment.objects.filter(page=page_name).order_by('-timestamp')
-    
-    return render(request, f'trips/{page_name}.html', {
-        'comments': comments,
-        'stats': stats,
-        'page_name': page_name,
-    })
+    """通用旅行页面渲染函数 - 重定向"""
+    return redirect_old_trip(request, page_name)
 
